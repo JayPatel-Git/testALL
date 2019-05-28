1ce24869f7d49031377c834826e31af5c26935d8
jay <jaj080128@gmail.com>
2019-05-28 12:56:06 -0500

    first initialization

---
+++ discription.js
@@ -0,0 +1,7 @@
+// first step:-- writing html css and js to render outputs.
+// step2 a:- takes an input field for class name or function name 
+// step 2:- writing a java script file reader that takes all the functions of each file and creates a html view of selection of functions and classes and creates a table structure of each functionalities and a run button and arguments details text box/boxes.
+// step 3:- on each test run a js file is executed based on all the test conditions to check. 
+// step 4:- each test could take set of arguments to compare with a callback name and calles that function and checks the output to be true and false and gives output to a table field next to the function runed.
+
+// file discription or path is given to the select mode ?? after above is build//.
---
+++ js-test/callingFunctions.js
@@ -0,0 +1,2 @@
+//each test could take set of arguments to compare with a callback name and calles that function and checks the output to be true and false and gives output to a table field next to the function runed.
+
