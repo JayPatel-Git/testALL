a4a38adb00ec5bcd67a03e6e56a923f0d6752acb
jay <jaj080128@gmail.com>
2019-05-29 12:28:28 -0500

    initialization

--- js-test/renderHtml.js
+++ js-test/renderHtml.js
@@ -1,5 +1,58 @@
+
+function run() {
+    let inFnc = document.querySelector('#name').value;
+        let result = null;
+        if(inFnc === 'empty') {
+            alert('Please select function.');
+        }   else {
+            let functionToRun = document.querySelector('.function-to-run').value;
+            console.log(typeof functionToRun);
+            if(functionToRun === '') {
+                alert('Please enter function name to run.');
+            }  else {
+                // if(!functionToRun.includes('()')) {
+                     
+                // }
+                try {
+                    result = eval(functionToRun);
+                }   catch (err){
+                    console.log(err.stack);
+                    result = err.stack;
+                }
+                    let display = document.querySelector('.result');
+                    display.textContent = result;
+                    display.style.color = 'green'; 
+            }
+            //if(functionToRun)
+        }
+}
+
+
 document.addEventListener('DOMContentLoaded', function(main) {
-    console.log('hi');
+    let runTest = document.querySelector('.run');
+    runTest.addEventListener('click', function(click) {
+        run();   
+    });
+    let hoverover = document.querySelector('#name');
+    hoverover.addEventListener('mouseover', function(e) {
+        if(e.target.classList.contains('typeMenu')) {
+            e.target.classList.remove('hidden');
+        }
+    });
+
 });
 
-console.log('hi !!!');
+
+
+// TypeError.prototype.message
+//     Error message. Although ECMA-262 specifies that TypeError should provide its own message property, in SpiderMonkey, it inherits Error.prototype.message.
+// TypeError.prototype.name
+//     Error name. Inherited from Error.
+// TypeError.prototype.fileName
+//     Path to file that raised this error. Inherited from Error.
+// TypeError.prototype.lineNumber
+//     Line number in file that raised this error. Inherited from Error.
+// TypeError.prototype.columnNumber
+//     Column number in line that raised this error. Inherited from Error.
+// TypeError.prototype.stack
+//     Stack trace. Inherited from Error.
---
+++ sample-test.js
@@ -0,0 +1,7 @@
+function a() {
+    return 2;
+}
+
+function b() {
+    return [1,2,3,4,5];
+}
